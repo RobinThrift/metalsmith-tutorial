58ba1d06938cc5fae76a070bd09fb03913474673
Robin Thrift <robin@we-two-design.com>
2014-04-18 16:46:13 +0200

    partialised header and footer templates

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 var Metalsmith = require('metalsmith'),
     markdown   = require('metalsmith-markdown'),
-    templates  = require('metalsmith-templates');
+    templates  = require('metalsmith-templates'),
+    Handlebars = require('handlebars'),
+    fs         = require('fs');
+
+
+Handlebars.registerPartial('header', fs.readFileSync(__dirname + '/templates/partials/header.hbt').toString());
+Handlebars.registerPartial('footer', fs.readFileSync(__dirname + '/templates/partials/footer.hbt').toString());
 
 
 Metalsmith(__dirname)
