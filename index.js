d72b5c4fde8956eefdf92f5c0736cb4d8795998c
Robin Thrift <robin@we-two-design.com>
2014-04-18 18:41:00 +0200

    added permalinks and collections

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-var Metalsmith = require('metalsmith'),
-    markdown   = require('metalsmith-markdown'),
-    templates  = require('metalsmith-templates'),
-    Handlebars = require('handlebars'),
-    fs         = require('fs');
+var Metalsmith  = require('metalsmith'),
+    markdown    = require('metalsmith-markdown'),
+    templates   = require('metalsmith-templates'),
+    collections = require('metalsmith-collections'),
+    permalinks  = require('metalsmith-permalinks'),
+    Handlebars  = require('handlebars'),
+    fs          = require('fs');
 
 
 Handlebars.registerPartial('header', fs.readFileSync(__dirname + '/templates/partials/header.hbt').toString());
@@ -10,7 +12,15 @@ Handlebars.registerPartial('footer', fs.readFileSync(__dirname + '/templates/par
 
 
 Metalsmith(__dirname)
+    .use(collections({
+        pages: {
+            pattern: 'content/pages/*.md'
+        }
+    }))
     .use(markdown())
+    .use(permalinks({
+        pattern: ':collection/:title'
+    }))
     .use(templates('handlebars'))
     .destination('./build')
     .build()
